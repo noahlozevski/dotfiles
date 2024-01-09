3b63383bca07b5237bb8c8038510fc344fe9ba13
noah lozevski <noahlozevski@gmail.com>
2024-01-09 00:41:42 -0500

    Add quick blacklist option to yabai

---
+++ yabai/.config/yabai/add_to_blacklist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Path to yabai configuration
+yabai_config=~/.config/yabai/yabairc
+
+# Get the name of the currently focused application
+frontApp=$(osascript -e 'tell application "System Events"' -e 'set frontApp to name of first application process whose frontmost is true' -e 'end tell')
+
+# Escape special characters for regex
+escapedApp=$(echo "$frontApp" | sed 's/[]\/$*.^[]/\\&/g')
+
+# Check if the app is already in the blacklist
+if ! grep -q "app=\"^$escapedApp\$\" manage=off" "$yabai_config"; then
+    # Add a rule to yabairc to blacklist the application
+    echo "yabai -m rule --add app=\"^$escapedApp\$\" manage=off" >> "$yabai_config"
+
+    # Reload yabai configuration
+    yabai -m config --load
+fi
