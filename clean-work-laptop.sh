968116c3a2fc0b93866f6d7b98233a05bf799c0b
noah lozevski <nlozevsk@amazon.com>
2024-02-27 19:32:07 -0800

    add clean script

---
+++ clean-work-laptop.sh
@@ -0,0 +1,6 @@
+
+# paths that store a lot of shit to delete
+
+rm -rf /usr/local/amazon/var/acme/cache/*
+rm -rf /Users/nlozevsk/Library/Application\ Support/Slack/logs/*
+rm -rf /Users/nlozevsk/.gradle/caches/5.6.4
