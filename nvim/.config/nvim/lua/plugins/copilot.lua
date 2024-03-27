1a6c3a40bb869069be4383f20a4d7dff0693a91f
noah lozevski <nlozevsk@amazon.com>
2024-03-26 22:41:25 -0700

    add option to enable copilot manually

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,23 @@
 return {
     'github/copilot.vim',
-    enabled = function()
-        if vim.g.is_work then
-            return false
-        else
-            return true
-        end
-    end,
+    lazy = true,
+    -- enabled = function()
+    --     if vim.g.is_work then
+    --         return false
+    --     else
+    --         return true
+    --     end
+    -- end,
     config = function()
         vim.api.nvim_set_keymap("i",
             "<C-Space>",
             'copilot#Accept("<CR>")',
             { silent = true, expr = true })
+    end,
+    init = function()
+        -- Starts the copilot client
+        function start_copilot()
+            vim.cmd("Lazy load copilot.vim")
+        end
     end
 }
