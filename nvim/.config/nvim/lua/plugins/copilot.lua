970534efce27c39edcd75d102f38b1bc1fb2884a
noah lozevski <nlozevsk@amazon.com>
2024-04-09 14:26:59 -0700

    fix copilot setup on lazy load

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -9,12 +9,16 @@ return {
     --     end
     -- end,
     config = function()
+        vim.g.copilot_assume_mapped = true
+
         vim.api.nvim_set_keymap("i",
             "<C-Space>",
             'copilot#Accept("<CR>")',
             { silent = true, expr = true })
     end,
     init = function()
+        vim.g.copilot_assume_mapped = true
+
         -- Starts the copilot client
         function start_copilot()
             vim.cmd("Lazy load copilot.vim")
