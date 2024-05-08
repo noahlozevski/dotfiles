78d3e0b96cf15773fc87e462b8652519660b8a3c
noah lozevski <nlozevsk@amazon.com>
2024-05-08 16:13:31 -0700

    update clean script

--- clean-work-laptop.sh
+++ clean-work-laptop.sh
@@ -1,6 +1,29 @@
+# rm -rf /usr/local/amazon/var/acme/cache/
+rm -rfv /Users/nlozevsk/Library/Application\ Support/Slack/logs/
+rm -rfv $HOME/.gradle/caches/
+rm -rfv ~/Library/Developer/Xcode/DerivedData/*
+rm -rfv ~/Library/Developer/CoreSimulator/Caches/*
+rm -rfv $HOME/Library/Messages/Caches/*
+npm cache clean --force --verbose
+# this cleans remote / private packages for some reason?
+brew cleanup --prune=all --verbose
+brazil-package-cache stop
+echo -n "Cleaning pkg cache ..." && brazil-package-cache clean --debug --days 0 --keepCacheHours 0 --percent 0 2>/dev/null
+rm -frv /local/home/thomadan/brazil-pkg-cache/s3BinaryFiles/*
+echo -n "Cleaning toolbox ..." && toolbox clean 2>&1
 
-# paths that store a lot of shit to delete
+# delete things from the ~/Library directory if over a size
+# MB
+threshold_size=500
 
-rm -rf /usr/local/amazon/var/acme/cache/*
-rm -rf /Users/nlozevsk/Library/Application\ Support/Slack/logs/*
-rm -rf /Users/nlozevsk/.gradle/caches/5.6.4
+# Path where to search directories. Change this to the path where you want to search.
+search_path="$HOME/Library/Caches"
+
+# Find directories larger than the specified threshold and delete them
+find "$search_path" -type d -mindepth 1 -maxdepth 1 -exec du -sm {} + | while read size path; do
+    if [ "$size" -gt $threshold_size ]; then
+        echo "Deleting directory $path which is $size MB"
+        rm -rf "$path"
+    fi
+done
+
