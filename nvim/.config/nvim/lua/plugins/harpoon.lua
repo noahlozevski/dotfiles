fc75e10f0d384808795e18d95159721460f00aaa
noah lozevski <noahlozevski@gmail.com>
2024-05-31 17:44:52 -0700

    harpoon -> v2

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,30 +1,38 @@
+local v2 = function()
+    local harpoon = require("harpoon")
+
+    harpoon:setup({
+        settings = {
+            save_on_toggle = false,
+            sync_on_ui_close = true,
+            key = function()
+                return vim.loop.cwd()
+            end,
+        },
+    })
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-;>", function() harpoon:list():select(5) end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+end
+
+
+
+
 return {
     'ThePrimeagen/harpoon',
-    config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
-
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-        vim.keymap.set("n", "<C-h>", function()
-            ui.nav_file(1)
-        end)
-        vim.keymap.set("n", "<C-j>", function()
-            ui.nav_file(2)
-        end)
-        vim.keymap.set("n", "<C-k>", function()
-            ui.nav_file(3)
-        end)
-        vim.keymap.set("n", "<C-l>", function()
-            ui.nav_file(4)
-        end)
-        -- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-
-        require("harpoon").setup({
-            menu = {
-                width = 120
-            }
-        })
-    end
+    branch = "harpoon2",
+    config = v2,
 }
