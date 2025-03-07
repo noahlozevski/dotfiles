0c3405108f34822092ec69240d83134e20f4aa27
noah lozevski <nlozevsk@amazon.com>
2025-03-07 03:49:17 -0800

    copilot starts

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,91 +1,90 @@
 return {
-    'zbirenbaum/copilot.lua',
-    lazy = true,
-    -- enabled = function()
-    --     if vim.g.is_work then
-    --         return false
-    --     else
-    --         return true
-    --     end
-    -- end,
-    config = function()
-        vim.g.copilot_assume_mapped = true
+  'zbirenbaum/copilot.lua',
+  lazy = true,
+  config = function()
+    vim.g.copilot_assume_mapped = true
+    vim.api.nvim_set_keymap("i", "<C-Space>", '<Plug>(copilot-accept-line)', { silent = true, expr = true })
+  end,
+  init = function()
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_enabled = false
 
-        vim.api.nvim_set_keymap("i",
-            "<C-Space>",
-            '<Plug>(copilot-accept-line)',
-            { silent = true, expr = true })
-    end,
-    init = function()
-        vim.g.copilot_assume_mapped = true
-        vim.g.copilot_enabled = false
-
-        -- Starts the copilot client
-        function start_copilot()
-            vim.cmd("Lazy load copilot.lua")
-            vim.g.copilot_enabled = true
-            require('copilot').setup({
-                panel = {
-                    enabled = true,
-                    auto_refresh = false,
-                    keymap = {
-                        jump_prev = "[[",
-                        jump_next = "]]",
-                        accept = "<C-y>",
-                        -- refresh = "gr",
-                        -- open = "<M-CR>"
-                    },
-                    layout = {
-                        position = "bottom", -- | top | left | right
-                        ratio = 0.4
-                    },
-                },
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    debounce = 75,
-                    keymap = {
-                        accept = "<C-y>",
-                        accept_word = false,
-                        accept_line = false,
-                        next = "<M-]>",
-                        prev = "<M-[>",
-                        dismiss = false
-                        -- dismiss = "<C-]>",
-                    },
-                },
-                filetypes = {
-                    yaml = false,
-                    markdown = false,
-                    help = false,
-                    gitcommit = false,
-                    gitrebase = false,
-                    hgcommit = false,
-                    svn = false,
-                    cvs = false,
-                    ["."] = false,
-                    ["Config"] = false
-                    -- ["*"] = false, disables all other files
-                },
-                copilot_node_command = 'node', -- Node.js version must be > 18.x
-                server_opts_overrides = {},
-            })
-        end
-
-        vim.api.nvim_create_user_command('CopilotStart', start_copilot, {})
-
-        -- if the the lua file at lozevski.work is callable, then call the start_copilot function
-        if vim.g.is_work then
-            -- load work config
-        else
-            -- auto start copilot on insert enter
-            vim.api.nvim_create_autocmd("InsertEnter", {
-                callback = function()
-                    vim.notify("strting copilot")
-                    start_copilot()
-                end,
-                group = vim.api.nvim_create_augroup("start_copilot", { clear = true }),
-            })
-        end
+    local function start_copilot()
+      vim.cmd("Lazy load copilot.lua")
+      vim.g.copilot_enabled = true
+      require('copilot').setup({
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<C-y>",
+          },
+          layout = {
+            position = "bottom",
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<C-y>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = false,
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+          ["Config"] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      })
     end
+
+    local allowedWorkspaces = {
+      "vision-mono-repo",
+      "dotfiles"
+    }
+    local currentWorkspace = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    local autoStart = false
+
+    if not vim.g.is_work then
+      autoStart = true
+    else
+      for _, ws in ipairs(allowedWorkspaces) do
+        if currentWorkspace == ws then
+          autoStart = true
+          break
+        end
+      end
+    end
+
+    if autoStart then
+      vim.api.nvim_create_autocmd("InsertEnter", {
+        callback = function()
+          vim.notify("starting copilot")
+          start_copilot()
+        end,
+        group = vim.api.nvim_create_augroup("start_copilot", { clear = true }),
+      })
+    else
+      vim.api.nvim_create_user_command('CopilotStart', start_copilot, {})
+    end
+  end
 }
+
