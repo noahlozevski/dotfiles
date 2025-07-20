484efef410ce930cfb06aafdfd42ebfba54d9f8c
noah lozevski <noahlozevski@gmail.com>
2025-07-19 19:55:30 -0700

    supermaven instead of copilot

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -91,6 +91,32 @@ return {
         html = { "prettierd", "prettier", stop_after_first = true },
       },
     },
+  },
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+        -- ignore_filetypes = { cpp = true }, -- or { "cpp", }
+        -- color = {
+        --   suggestion_color = "#ffffff",
+        --   cterm = 244,
+        -- },
+        log_level = "info",                -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false,           -- disables built in keymaps for more manual control
+        -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+        condition = function()
+          if os.getenv("SUPERMAVEN_DISABLED") == "1" or vim.fn.expand("%:t") == "nlozevsk" then
+            return true
+          end
+          return false
+        end
+      })
+    end,
   }
-
 }
